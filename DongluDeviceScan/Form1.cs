7a148692265536ebf3fea2e913408a9a09a50118
panmingzhi <154341736@qq.com>
2016-11-14 15:58:02 +0800

    完善TFTP的上传

--- DongluDeviceScan/Form1.cs
+++ DongluDeviceScan/Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
+using Tftp.Net;
+using System.Threading;
+using System.IO;
 
 namespace DongluDeviceScan
 {
@@ -14,13 +13,21 @@ namespace DongluDeviceScan
     {
 
         private NetWorker netUtil = new NetWorker();
-        
+        private static AutoResetEvent TransferFinishedEvent = new AutoResetEvent(false);
+        private string key;
+        private List<string> ipList;
+
         public Form1()
         {
             InitializeComponent();
+            skinTextBox1.Text = "192.168.1.1";
+            skinTextBox2.Text = "192.168.1.255";
+            skinTextBox3.Text = "";
+            skinTextBox4.Text = "";
         }
 
-        public void initTreeNode() {
+        public void initTreeNode()
+        {
             skinTreeView1.Nodes.Clear();
             TreeNode dongluNode = new TreeNode("东陆设备(0个)");
             TreeNode unKnowNode = new TreeNode("未知设备(0个)");
@@ -38,45 +45,217 @@ namespace DongluDeviceScan
 
         private void _myPing_PingCompleted(object sender, PingCompletedEventArgs e)
         {
-            if (!e.UserState.Equals(skinProgressBar1.Name)) {
+           
+            Console.WriteLine("UserState:" + e.UserState);
+            if (!e.UserState.Equals(skinProgressBar1.Name))
+            {
                 return;
             }
+            BeginInvoke((new EventHandler(delegate (object o, EventArgs ea)
+            {
+                skinProgressBar1.Value = skinProgressBar1.Value + 1;
+            })));
             if (e.Reply.Status == IPStatus.Success)
             {
                 string ip = e.Reply.Address.ToString();
-                skinTreeView1.TopNode.Nodes.Add(new TreeNode(ip + "[" + netUtil.getMacAddress(ip) + "]"));
-                skinTreeView1.TopNode.Text = "东陆设备(" + skinTreeView1.TopNode.Nodes.Count + "个)";
-                Console.WriteLine("success : " + e.Reply.Address.ToString());
+                Thread newthread = new Thread(new ParameterizedThreadStart(macThread));
+                newthread.Start(ip);
             }
-            skinProgressBar1.Value = skinProgressBar1.Value + 1;
+
         }
+
+        private void macThread(object obj)
+        {
+            string ip = obj as string;
+            string mack_key = netUtil.getMacAddress(ip);
+            String deviceMac = parseDeviceMac(mack_key.Substring(0, 5));
+            if (deviceMac.Length == 0)
+            {
+                BeginInvoke((new EventHandler(delegate (object o, EventArgs ea)
+                {
+                    TreeNode td = skinTreeView1.Nodes[1];
+                    td.Nodes.Add(new TreeNode(ip + "[" + netUtil.getMacAddress(ip) + "]"));
+                    td.Text = "未知设备(" + td.Nodes.Count + "个)";
+                })));
+            }
+            else
+            {
+                BeginInvoke((new EventHandler(delegate (object o, EventArgs ea)
+                {
+                    TreeNode td = skinTreeView1.Nodes[0];
+                    td.Nodes.Add(new TreeNode(ip + "[" + netUtil.getMacAddress(ip) + "]" + deviceMac));
+                    td.Text = "东陆设备(" + td.Nodes.Count + "个)";
+                })));
+
+            }
+        }
+
+        public string parseDeviceMac(string mac_key)
+        {
+            if (mac_key == null)
+            {
+                return "";
+            }
+            String mac_name;
+            switch (mac_key)
+            {
+                case "B4:AA":
+                    mac_name = "单门控制器";
+                    break;
+                case "B4:BB":
+                    mac_name = "双门单项控制器";
+                    break;
+                case "B4:CC":
+                    mac_name = "双门双向控制器";
+                    break;
+                case "B4:DD":
+                    mac_name = "四门控制器";
+                    break;
+                case "B4:EE":
+                    mac_name = "窗口扣费POS机";
+                    break;
+                case "B4:FF":
+                    mac_name = "小票扣费POS机";
+                    break;
+                case "B4:11":
+                    mac_name = "IC卡电梯控制器";
+                    break;
+                case "B4:22":
+                    mac_name = "闸机门禁控制器";
+                    break;
+                case "B4:33":
+                    mac_name = "停车场4000";
+                    break;
+                case "B4:44":
+                    mac_name = "停车场2000";
+                    break;
+                case "B4:55":
+                    mac_name = "双层票箱";
+                    break;
+                default:
+                    mac_name = "";
+                    break;
+            }
+            return mac_name;
+        }
+
 
         private void skinButton1_Click(object sender, EventArgs e)
         {
             initTreeNode();
-            netUtil.setScanIp(skinTextBox1.Text,skinTextBox2.Text);
-            List<string> ipList = netUtil.getAllScanIp();
+            this.ipList = netUtil.getAllScanIp(skinTextBox1.Text, skinTextBox2.Text);
 
-            string key = DateTime.Now.ToLongTimeString();
+            this.key = DateTime.Now.ToLongTimeString();
             skinProgressBar1.Name = key;
             skinProgressBar1.Maximum = ipList.Count();
             skinProgressBar1.Minimum = 0;
             skinProgressBar1.Value = 0;
 
-            foreach (string s in ipList)
-            {
-                netUtil.checkIp(s, key, new System.Net.NetworkInformation.PingCompletedEventHandler(_myPing_PingCompleted));
-            }
+            Thread newthread = new Thread(new ThreadStart(macThread));
+            newthread.Start();
+            int start = System.DateTime.Now.Millisecond;
+           
         }
 
-        private void skinButton2_Click(object sender, EventArgs e)
+        private void macThread()
         {
+            foreach (string s in ipList)
+            {
+                Console.WriteLine("key:" + key + "  s:" + s);
+                netUtil.checkIp(s, key, new System.Net.NetworkInformation.PingCompletedEventHandler(_myPing_PingCompleted));
+            }
 
         }
 
         private void afterCheck(object sender, TreeViewEventArgs e)
         {
             TreeViewChecker.CheckControl(e);
+        }
+
+        private void skinButton2_Click(object sender, EventArgs e)
+        {
+            if (skinTextBox3.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("请选中一个设备", "提示");
+                return;
+            }
+            if (skinTextBox4.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("请选择升级文件", "提示");
+                return;
+            }
+            tftpDownLoad(skinTextBox3.Text, skinTextBox4.Text);
+        }
+
+        public void tftpDownLoad(string ip, string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            skinProgressBar1.Maximum = (int)fs.Length;
+            skinProgressBar1.Value = skinProgressBar1.Maximum / 4;
+            Console.WriteLine("下载文件：" + fileName);
+            Console.WriteLine("下载设备：" + ip);
+            netUtil.iap(ip, 10001);
+            skinProgressBar1.Value = skinProgressBar1.Maximum / 2;
+
+            var client = new TftpClient(ip, 10001);
+
+            var transfer = client.Upload(fileName);
+            transfer.TransferMode = TftpTransferMode.octet;
+
+            transfer.OnProgress += new TftpProgressHandler(transfer_OnProgress);
+            transfer.OnFinished += new TftpEventHandler(transfer_OnFinshed);
+            transfer.OnError += new TftpErrorHandler(transfer_OnError);
+
+            transfer.Start(fs);
+
+            TransferFinishedEvent.WaitOne();
+        }
+
+        void transfer_OnProgress(ITftpTransfer transfer, TftpTransferProgress progress)
+        {
+            BeginInvoke((new EventHandler(delegate (object o, EventArgs e)
+            {
+                skinProgressBar1.Value = progress.TransferredBytes;
+            })));
+        }
+
+
+        void transfer_OnError(ITftpTransfer transfer, TftpTransferError error)
+        {
+            Console.WriteLine("Transfer failed: " + error);
+            TransferFinishedEvent.Set();
+            MessageBox.Show("升级失败", "提示");
+        }
+
+        void transfer_OnFinshed(ITftpTransfer transfer)
+        {
+            Console.WriteLine("Transfer succeeded.");
+            TransferFinishedEvent.Set();
+            MessageBox.Show("升级成功", "提示");
+        }
+
+        private void skinTreeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                string nodeText = e.Node.Text;
+                skinTextBox3.Text = nodeText.Substring(0, nodeText.IndexOf("["));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        private void skinButton3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "bin文件(*.bin)|*.*";
+            openFileDialog1.FileName = "";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName.EndsWith(".bin"))
+            {
+                skinTextBox4.Text = openFileDialog1.FileName; 
+            }
         }
     }
 }
--- DongluDeviceScan/util/NetWorker.cs
+++ DongluDeviceScan/util/NetWorker.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace DongluDeviceScan
 {
@@ -16,12 +17,22 @@ namespace DongluDeviceScan
         [DllImport("IPHLPAPI.dll")]
         private static extern int SendARP(Int32 DestIP, Int32 SrcIP, ref Int64 pMacAddr, ref Int32 PhyAddrLen);
 
-        private string startIp = "192.168.1.1";
-        private string endIp = "192.168.1.255";
+        private byte[] sendMessage = { 0x01, 0x57, 0x00, 0x01, 0x00, 0x01, 0x04, 0x02, 0x44, 0x4C, 0X32, 0X30, 0X31, 0X35, 0X30, 0X31, 0X32, 0X30, 0X03, 0X00 };
 
-        public void setScanIp(string startIp,string endIp) {
-            this.startIp = startIp;
-            this.endIp = endIp;
+        public void iap(String ip, int port) {
+            Console.WriteLine("进入iap 命令 " + ip + ":" + port);
+            try
+            {
+                ///创建socket并连接到服务器
+                Socket c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                c.Connect(ip, port);
+                c.Send(sendMessage);
+                c.Close(2000);
+            }
+            catch (Exception e) {
+                Console.WriteLine("iap 进入失败！");
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void checkIp(string ip,string key, PingCompletedEventHandler asynEvent)
@@ -31,7 +42,7 @@ namespace DongluDeviceScan
                 IPAddress address = IPAddress.Parse(ip);
                 Ping ping = new Ping();
                 ping.PingCompleted += asynEvent;
-                ping.SendAsync(address, 3000, key);
+                ping.SendAsync(address, 1000, key);
             }
             catch (Exception e) {
                 Console.WriteLine(e.Message);
@@ -40,6 +51,8 @@ namespace DongluDeviceScan
 
         public string getMacAddress(string IpAddress)
         {
+
+            int start = System.DateTime.Now.Millisecond;
             string macAddress = "";
             Int32 ldest = 0;
             try
@@ -79,21 +92,25 @@ namespace DongluDeviceScan
                 macAddress = "无法探测到MAC地址";
                 //canPing = false;
             }
+            Console.WriteLine("探测MAC地址耗时：" + (System.DateTime.Now.Millisecond - start));
             return macAddress;
         }
 
-        public List<string> getAllScanIp() {
-            int endPoint1 = this.startIp.LastIndexOf(".") + 1;
-            int endPoint2 = this.endIp.LastIndexOf(".") + 1;
+        public List<string> getAllScanIp(string startIp,string endIp) {
+            Console.WriteLine("获取所有ip段：" + startIp + "--" + endIp);
 
-            string start1 = this.startIp.Substring(0, endPoint1);
-            string start2 = this.endIp.Substring(0, endPoint2);
+            int start = System.DateTime.Now.Millisecond;
+            int endPoint1 = startIp.LastIndexOf(".") + 1;
+            int endPoint2 = endIp.LastIndexOf(".") + 1;
+
+            string start1 = startIp.Substring(0, endPoint1);
+            string start2 = endIp.Substring(0, endPoint2);
             if (!start1.Equals(start2)) {
                 return new List<string>();
             }
 
-            int end1 = Int32.Parse(this.startIp.Substring(endPoint1));
-            int end2 = Int32.Parse(this.endIp.Substring(endPoint2));
+            int end1 = Int32.Parse(startIp.Substring(endPoint1));
+            int end2 = Int32.Parse(endIp.Substring(endPoint2));
             if (end1 > end2) {
                 return new List<string>();
             }
@@ -102,6 +119,7 @@ namespace DongluDeviceScan
             for (int i = end1; i < end2; i++) {
                 resultList.Add(start1 + i);
             }
+            Console.WriteLine("获取所有ip段耗时（毫秒）：" + (System.DateTime.Now.Millisecond - start));
             return resultList;
         }
 
